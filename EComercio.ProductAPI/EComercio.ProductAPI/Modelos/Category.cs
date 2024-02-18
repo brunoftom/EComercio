5f7d153ce496c58cc244638be7c72203d448c107
Bruno Ferreira <49572026+brunoftom@users.noreply.github.com>
2024-02-18 16:56:34 -0300

    Add files via upload

---
+++ EComercio.ProductAPI/EComercio.ProductAPI/Modelos/Category.cs
@@ -0,0 +1,10 @@
+﻿namespace EComercio.ProductAPI.Modelos;
+
+public class Category
+{
+    public int CategoryId { get; set; } 
+    public string? Name { get; set; }
+
+    public ICollection<Product>? Products { get; set; }  
+
+}
